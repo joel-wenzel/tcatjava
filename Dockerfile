c2555826ada8e9fcc1a5a675e5265e5536b8b716
Eugene Fedorenko <eugene.fedorenko@flexagon.com>
2018-09-10 12:13:36 -0500

    olympics-mw

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM eugeneflexagon/adftomcat:1.0
-COPY tcatapp.war /usr/local/tomcat/webapps
+FROM eugeneflexagon/adftomcat:2.0
+COPY tcatapp.war /usr/local/tomcat/webapps/
--- ViewController/src/view/MedalRatingService.java
+++ ViewController/src/view/MedalRatingService.java
@@ -6,10 +6,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import model.MedalsVORowImpl;
+
+import model.common.AppModule;
+
+import oracle.jbo.Row;
+import oracle.jbo.client.Configuration;
+
 @Path("/medalrating")
 public class MedalRatingService
 {
-  private static MedalStatList medalStatList = new MedalStatList();
+  private MedalStatList medalStatList;
+
+  /*= new MedalStatList();
   static
   {
     medalStatList.add(MedalStat.getInstance("no", "Norway", 1, 14, 14, 11));
@@ -17,7 +26,7 @@ public class MedalRatingService
     medalStatList.add(MedalStat.getInstance("ca", "Canada", 3, 11, 8, 10));
     medalStatList.add(MedalStat.getInstance("us", "USA", 4, 9, 8, 6));
     medalStatList.add(MedalStat.getInstance("nl", "Netherands", 5, 8, 6, 6));
-  }
+  }*/
 
   public MedalRatingService()
   {
@@ -29,6 +38,28 @@ public class MedalRatingService
   @Produces("application/json")
   public List<MedalStat> getRatingTable()
   {
+    medalStatList = new MedalStatList();
+    AppModule am = null;
+    int l = 0;
+
+    try
+    {
+      am = (AppModule) Configuration.createRootApplicationModule("model.AppModule", "AppModuleLocal");
+      List<Row> list = am.getMedals();
+      int i = 1;
+      for (Row r: list)
+      {
+        MedalsVORowImpl row = (MedalsVORowImpl) r;
+        medalStatList.add(MedalStat.getInstance(row.getCountryid(), row.getCountryname(), i++, row.getGold(), row.getSilver(), row.getBronze()));
+      }
+    }
+    finally
+    {
+      if (am != null)
+        Configuration.releaseRootApplicationModule(am, false);
+    }
+
+
     return medalStatList.getList();
   }
 
